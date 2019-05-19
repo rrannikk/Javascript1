20a4e8ebe7a5d66af18f2763d073ba3e4566a882
Richard Rannikko <richard@bizintl.net>
2019-05-19 17:09:10 +0200

    Add files via upload

---
+++ dogyears.js
@@ -0,0 +1,18 @@
+// this is my age
+const myAge = 37;
+// early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+// dont know what to comment
+let laterYears = myAge - 2;
+//a dogs ageing after the second year
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+//my age in dog years
+let myAgeInDogYears = laterYears + earlyYears;
+
+let myName = 'Richard'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
